ab84b40f846eac3c3b1d857f681cc606dfb733c0
Martin Mullen <martin_mullen@hotmail.com>
2021-01-31 19:39:15 -0700

    React Week 3 Post Presentational and Container Components Exercise

---
+++ src/components/CampsiteInfoComponents.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+    }
+
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <div>
+                        {comments.map(comments => {
+                            return (<div><p>{comments.text} <br/>-- {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</p></div>)
+                        })
+                        }
+                    </div>
+                </div>
+            )
+            return <div></div>
+        }
+    }
+
+    render() {
+        if (this.props.campsite) {
+            return (
+            <div className="container">
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            </div>
+            );
+        }
+        return <div></div>
+    }
+
+
+}
+
+export default CampsiteInfo;
