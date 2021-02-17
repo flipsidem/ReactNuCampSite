681cc5129c8faaf92f3d56f02d8af565a45fed3a
Martin Mullen <martin_mullen@hotmail.com>
2021-02-16 22:20:14 -0700

    React Week 5 Post Redux Thunk Exercise

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,7 @@
 import * as ActionTypes from './ActionTypes';
+import { CAMPSITES } from '../shared/campsites';
+import { actionTypes } from 'react-redux-form';
+
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +11,27 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author: author,
         text: text
     }
-})
+});
+
+export const fetchCampsites = () => dispatch => {
+   
+    dispatch(campsitesLoading());
+   
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+};
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
+});
